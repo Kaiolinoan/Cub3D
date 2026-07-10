70fbf11847144119cdd9fa9aa4953ed099152706
Kaio <kaiolinoan@gmail.com>
2026-07-10 11:36:44 +0100

    norm

--- srcs/main.c
+++ srcs/main.c
@@ -23,8 +23,9 @@ int	main(int argc, char **argv)
 		return (1);
 	if (!check_if_map_is_valid(argv[1], game))
 		return (clear_game(game), 1);
-	// game->debug = true;
 	mlx_main(game);
 	clear_game(game);
 	return (0);
 }
+
+	// game->debug = true;
